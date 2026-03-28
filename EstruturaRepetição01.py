19f516be7588f0ef5254f4184e6b041edcf9c7f8
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:59:40 -0300

    .

--- EstruturaRepetição01.py
+++ EstruturaRepetição01.py
@@ -1,3 +1,6 @@
+#Exercício 1
+#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
 while True:
     numero = int(input('Informe um número entre 0 e 10: '))
     if numero > 10:
