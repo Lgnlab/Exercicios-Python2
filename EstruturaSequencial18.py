1110391d1be50ca826d5eea287e0d3c064d8b724
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:28:42 -0300

    .

--- EstruturaSequencial18.py
+++ EstruturaSequencial18.py
@@ -1,3 +1,7 @@
+#Exercício 18
+#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps),
+#calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+
 arquivo = float(input('Tamanho Arquivo(MB): '))
 velocidade = float(input('Velocidade Da Internet(Mbps): '))
 calculo = arquivo * 8           #Converter para mb
