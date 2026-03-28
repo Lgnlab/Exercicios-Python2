735ebac807d073118a6afff6e48b00e0aaeb3d35
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:33:52 -0300

    .

--- EstruturaDecisão03.py
+++ EstruturaDecisão03.py
@@ -1,3 +1,10 @@
+#Exercício 03
+#Faça um programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever:
+
+#F - Feminino
+#M - Masculino
+#Sexo Inválido.
+
 sexo = str(input('Sexo: ')).strip().upper()[0]
 
 if sexo == 'F':
