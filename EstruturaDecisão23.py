6d41bfc509a551d86c6bb26935da9b2f87ce8bbc
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:49:59 -0300

    .

--- EstruturaDecisão23.py
+++ EstruturaDecisão23.py
@@ -1,3 +1,6 @@
+#Exercício 23
+#Faça um programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+
 numero = float(input('Informe Um Número: '))
 if numero % 1 == 0:
     print(f'O número {numero:.0f} é INTEIRO!')
