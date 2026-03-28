8574a2cd90a5006db89c2236408a3bb7f286005e
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:39:43 -0300

    .

--- EstruturaDecisão10.py
+++ EstruturaDecisão10.py
@@ -1,3 +1,11 @@
+#Exercício 10
+#Faça um programa que pergunte em que turno você estuda. Peça para digitar:
+
+#M - Matutino
+#V - Vespertino
+#N - Noturno.
+#Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
 turno = str(input('Informe o turno que você estuda (Matutino/Vespertino/Noturno): ')).strip().upper()[0]
 if turno == 'M':
     print('Bom dia!')
