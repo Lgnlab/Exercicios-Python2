134f1ac178e8330f144588cf4c91fc8f8b15bcfb
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 20:10:46 -0300

    .

--- Strings04.py
+++ Strings04.py
@@ -1,3 +1,6 @@
+#Exercício 04
+#Nome na vertical em escada. Modifique o programa anterior de forma a mostrar o nome em formato de escada.
+
 nome = str(input('Digite seu nome: ')).upper()
 for letra in range(1, len(nome) + 1):
     print(nome[:letra])
