09c390011e302b00eda1fecc39434303a4ea4ffd
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:14:06 -0300

    .

--- EstruturaSequencial02.py
+++ EstruturaSequencial02.py
@@ -1,2 +1,5 @@
+#Exercício 02
+#Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
+
 numero = int(input('Informe um número: '))
 print(f'O número informado foi {numero}')
