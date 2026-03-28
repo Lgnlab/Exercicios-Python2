209c04f06362f354951b628c7357685b45b55f1d
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:22:46 -0300

    .

--- EstruturaSequencial13.py
+++ EstruturaSequencial13.py
@@ -1,3 +1,10 @@
+#Exercício 13
+#Tendo como dados de entrada um arquivo em Gigabytes, construa um algoritmo que faça a conversão para Megabytes e Kilobytes, usando as seguintes fórmulas:
+
+#Para Megabytes: Gigabytes * 1024
+#Para Kilobytes: Gigabytes * 1024 * 1024
+#Responda o tamanho do arquivo em Megabytes e o tamanho em Kilobytes
+
 gigaby = float(input('Arquivo em Gigabytes: GB'))
 print(f'Tamanho do arquivo em Megabytes: {gigaby * 1024}MG')
 print(f'Tamanho do arquivo em Kilobytes: {gigaby * 1024 * 1024}')
