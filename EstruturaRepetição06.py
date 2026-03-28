e06a29e42a22534631a151a663e511e31074a5d6
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 20:03:07 -0300

    .

--- EstruturaRepetição06.py
+++ EstruturaRepetição06.py
@@ -1,2 +1,5 @@
+#Exercício 6
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
 for n in range(1, 20 + 1):
     print(n, end=' ')
