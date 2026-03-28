3a4e97b7b6b38166888ec24d3f53ae60d57c5051
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:21:59 -0300

    .

--- EstruturaSequencial12.py
+++ EstruturaSequencial12.py
@@ -1,2 +1,5 @@
+#Exercício 12
+#Tendo como dados de entrada um arquivo em Gigabytes, construa um algoritmo que faça a conversão para Megabytes, usando a seguinte fórmula:
+
 gigabytes = float(input('Informe o tamanho do arquivo: GB'))
 print(f'Convertendo o arquivo {gigabytes}GB para megabytes: {gigabytes * 1024}MB')
