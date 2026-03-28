f77e8803376c25529d4f97c15f464712cf47cea6
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 20:04:36 -0300

    .

--- EstruturaRepetição09.py
+++ EstruturaRepetição09.py
@@ -1,3 +1,6 @@
+#Exercício 9
+#Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
 print(f'Números ímpares no intervalo de 1 a 50:')
 for c in range(1, 50, 2):
     print(c, end=' ')
