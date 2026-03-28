77c2ccffd0aa72ba06bb867f1d8eaba1e76db9f3
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 20:02:10 -0300

    .

--- EstruturaRepetição04.py
+++ EstruturaRepetição04.py
@@ -1,3 +1,7 @@
+#Exercício 4
+#Supondo que a população de um país A seja da ordem de 80_000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200_000 habitantes com uma taxa de crescimento de 1.5%.
+#Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
 pessoas_a = 80000
 pessoas_b = 200000
 anos = 0
