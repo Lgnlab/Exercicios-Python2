e7514af2d975a196d7db2d71b43721f122c4eb73
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:21:15 -0300

    .

--- EstruturaSequencial11.py
+++ EstruturaSequencial11.py
@@ -1,3 +1,10 @@
+#Exercício 11
+#Faça um programa que peça 2 números inteiros e um número real. Calcule e mostre:
+
+#O produto do dobro do primeiro com metade do segundo .
+#A soma do triplo do primeiro com o terceiro.
+#O terceiro elevado ao cubo.
+
 num1 = int(input('Primeiro número: '))
 num2 = int(input('Segundo número: '))
 num3 = float(input('Terceiro número: '))
