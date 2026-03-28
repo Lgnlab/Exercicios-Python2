64a1fe4fc99ec0cc248b9f7b6052632a070918ef
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:26:42 -0300

    .

--- EstruturaSequencial16.py
+++ EstruturaSequencial16.py
@@ -1,3 +1,8 @@
+#Exercício 16
+#Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+#Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
+#Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+
 import math
 from math import ceil
 
