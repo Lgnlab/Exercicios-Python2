daf8ebb262b5b8c5866628d90f9c136ff0b22b85
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 19:52:06 -0300

    .

--- EstruturaDecisão25.py
+++ EstruturaDecisão25.py
@@ -1,3 +1,15 @@
+#Exercício 25
+#Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+
+#"Telefonou para a vítima?"
+#"Esteve no local do crime?"
+#"Mora perto da vítima?"
+#"Devia para a vítima?"
+#"Já trabalhou com a vítima?"
+#O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
+#Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino".
+#Caso contrário, ele será classificado como "Inocente".
+
 positivo = 0
 print('Responda com [S/N]:')
 p1 = str(input('Telefonou para a vítima? ')).strip().upper()[0]
