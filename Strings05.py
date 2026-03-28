0dea0dcec7ca85958cfe9077c8a10e39edfb8572
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-28 20:11:19 -0300

    .

--- Strings05.py
+++ Strings05.py
@@ -1,3 +1,6 @@
+#Exercício 05
+#Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+
 nome = str(input('Digite seu nome: ')).upper()
 for i in range(len(nome), 0 , -1):
     print(nome[:i])
