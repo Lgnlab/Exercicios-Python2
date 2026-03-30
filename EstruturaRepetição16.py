01364f140a3ac839334477bdf613ac1bac296753
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-30 20:10:03 -0300

    .

---
+++ EstruturaRepetição16.py
@@ -0,0 +1,13 @@
+#Exercício 16
+#A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até que o valor seja maior que 500.
+
+t1 = 0
+t2 = 1
+cont = 3
+print(f'{t1} -> {t2}', end='')
+while cont <=16:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
