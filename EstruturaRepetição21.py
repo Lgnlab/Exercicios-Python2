e16a4599e44f7dbe26699d30021bde29bb7ed909
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-03-30 22:43:33 -0300

    .

--- EstruturaRepetição21.py
+++ EstruturaRepetição21.py
@@ -1,12 +1,13 @@
 #Exercício 21
 #Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
 
-while True:
-    numero = int(input('Informe um número: '))
-    if numero % 2 == 0:
-        print(f'O número {numero} É PRIMO')
-    else:
-        print(f'O número {numero} NÃO É PRIMO!')
-    sair = input('Quer verificar outro número [S/N]: ').strip().upper()[0]
-    if sair == 'N':
-        break
+cont = 0
+numero = int(input('Informe um número: '))
+for p in range(1, numero + 1):
+    if numero % p == 0:
+        cont += 1
+if cont == 2:
+    print(f'O número {numero} É PRIMO')
+else:
+    print(f'O número {numero} NÃO É PRIMO!')
+
